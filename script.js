7a6a798cf2255dfe260b219789d7c8fdf191beda
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-26 23:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var isDate = function (input) {
-  //   write your code here
+  return !isNaN(Date.parse(input))
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+// const input = prompt("Enter Date.");
 alert(isDate(input));
